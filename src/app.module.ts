0e89ab881bbba2fb6d7da55411b1144bcbae6769
John Marvin Doniña <jbmarvin24@gmail.com>
2023-08-21 11:48:37 +0800

    Implement app config in orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,45 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
 import { ShopModule } from './shop/shop.module';
-import { Shop } from './shop/entities/shop.entity';
 import { CategoryModule } from './category/category.module';
-import { Category } from './category/entities/category.entity';
 import { VoucherTypeModule } from './voucher-type/voucher-type.module';
-import { VoucherType } from './voucher-type/entities/voucher-type.entity';
 import { VoucherModule } from './voucher/voucher.module';
-import { Voucher } from './voucher/entities/voucher.entity';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
-import { Profile } from './profile/entities/profile.entity';
 import { ProductModule } from './product/product.module';
-import { Product } from './product/entities/product.entity';
 import { WishlistModule } from './wishlist/wishlist.module';
-import { Wishlist } from './wishlist/entities/wishlist.entity';
 import { SearchModule } from './search/search.module';
+import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'batman@dev',
-      database: 'marvs_ecommerce',
-      entities: [
-        User,
-        Profile,
-        Shop,
-        Category,
-        VoucherType,
-        Voucher,
-        Product,
-        Wishlist,
-      ],
-      synchronize: true,
-      // dropSchema: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true,
+      envFilePath: `${process.env.NODE_ENV}.env`,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
     }),
     AuthModule,
     UserModule,
---
+++ src/config/orm.config.prod.ts
@@ -0,0 +1,34 @@
+import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Category } from '../category/entities/category.entity';
+import { Product } from '../product/entities/product.entity';
+import { Profile } from '../profile/entities/profile.entity';
+import { Shop } from '../shop/entities/shop.entity';
+import { User } from '../user/entities/user.entity';
+import { VoucherType } from '../voucher-type/entities/voucher-type.entity';
+import { Voucher } from '../voucher/entities/voucher.entity';
+import { Wishlist } from '../wishlist/entities/wishlist.entity';
+
+export default registerAs(
+  'orm.config',
+  (): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [
+      User,
+      Profile,
+      Shop,
+      Category,
+      VoucherType,
+      Voucher,
+      Product,
+      Wishlist,
+    ],
+    synchronize: false,
+    dropSchema: false,
+  }),
+);
