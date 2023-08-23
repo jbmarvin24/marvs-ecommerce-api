0adb79843f0c12064b93756a89a63585935f7832
John Marvin Doniña <jbmarvin24@gmail.com>
2023-08-23 19:39:29 +0800

    Include profile properties in registration

--- src/profile/dto/address.dto.ts
+++ src/profile/dto/address.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class AddressDto {
+  @ApiProperty({ example: 'Rizal' })
   @MaxLength(100)
   @IsNotEmpty()
   province: string;
 
+  @ApiProperty({ example: 'Cainta' })
   @MaxLength(100)
   @IsNotEmpty()
   city: string;
 
+  @ApiProperty({ example: 'St. Domingo' })
   @MaxLength(100)
   @IsNotEmpty()
   barangay: string;
 
+  @ApiProperty({ example: '445 My street address' })
   @MaxLength(200)
   @IsNotEmpty()
   street: string;
--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -3,30 +3,38 @@ import {
   MaxLength,
   IsOptional,
   ValidateNested,
-  IsJSON,
   IsNotEmptyObject,
 } from 'class-validator';
 import { AddressDto } from './address.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProfileDto {
+  @ApiProperty({ example: 'John' })
   @IsNotEmpty()
   @MaxLength(100)
   firstName: string;
 
+  @ApiProperty({ example: 'Doe' })
   @IsNotEmpty()
   @MaxLength(100)
   lastName: string;
 
+  @ApiProperty({ example: 'Pedro' })
   @IsOptional()
   @MaxLength(500)
   middleName?: string;
 
+  @ApiProperty({
+    description: 'Shipping address in a object',
+    type: AddressDto,
+  })
   @ValidateNested()
   @IsNotEmptyObject()
   @Type(() => AddressDto)
   shippingAddress: AddressDto;
 
+  @ApiProperty({ example: '5555555' })
   @IsNotEmpty()
   @MaxLength(100)
   phoneNo: string;
