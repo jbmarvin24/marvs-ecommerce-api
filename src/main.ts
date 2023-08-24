d2ee48c9f582ce21dabb9aeecbff93472ca492d3
John Marvin Doniña <jbmarvin24@gmail.com>
2023-08-24 21:14:41 +0800

    Add server to docs

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,7 @@ async function bootstrap() {
     )
     .setVersion('1.0')
     .addBearerAuth()
+    .addServer('http://localhost:4000')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
