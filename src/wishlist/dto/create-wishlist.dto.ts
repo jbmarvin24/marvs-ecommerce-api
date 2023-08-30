437c2472f957e3a0ab63eecd2a48d13fc2449e68
John Marvin Doniña <jbmarvin24@gmail.com>
2023-08-30 20:19:55 +0800

    Fixed minimum required

--- src/wishlist/dto/create-wishlist.dto.ts
+++ src/wishlist/dto/create-wishlist.dto.ts
@@ -3,7 +3,7 @@ import { IsNumber, Min } from 'class-validator';
 
 export class CreateWishlistDto {
   @ApiProperty({ description: 'Product Id', example: 1 })
-  @Min(0)
+  @Min(1)
   @IsNumber()
   productId: number;
 }
